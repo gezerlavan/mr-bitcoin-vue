8559890f376e80ac50795fb9f372873eebb26ad9
gezerlavan <dev.talelias@gmail.com>
2025-01-05 22:32:40 +0200

    Add services

---
+++ src/services/bitcoinService.js
@@ -0,0 +1,28 @@
+import axios from 'axios'
+
+export const bitcoinService = {
+    getRate,
+    getMarketPriceHistory,
+    getAvgBlockSize,
+}
+
+async function getRate() {
+    const res = await axios.get(
+        'https://blockchain.info/tobtc?currency=USD&value=1'
+    )
+    console.log('res.data:', res.data)
+}
+
+async function getMarketPriceHistory() {
+    const res = await axios.get(
+        'https://api.blockchain.info/charts/market-price?timespan=5months&format=json&cors=true'
+    )
+    console.log('res.data:', res.data)
+}
+
+async function getAvgBlockSize() {
+    const res = await axios.get(
+        'https://api.blockchain.info/charts/avg-block-size?timespan=5months&format=json&cors=true'
+    )
+    console.log('res.data:', res.data)
+}
---
+++ src/services/userService.js
@@ -0,0 +1,13 @@
+export const userService = {
+    getUser,
+}
+
+const user = {
+    name: 'Puki Ben David',
+    balance: 100,
+    transactions: [],
+}
+
+function getUser() {
+    return user
+}
