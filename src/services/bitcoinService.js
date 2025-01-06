a336ddc12fdc7c0083b59bca5978a9a0ffc52dce
gezerlavan <dev.talelias@gmail.com>
2025-01-06 19:29:10 +0200

    Implement local storage caching

--- src/services/bitcoinService.js
+++ src/services/bitcoinService.js
@@ -1,4 +1,7 @@
 import axios from 'axios'
+import { storageService } from './storage.service'
+
+const STORAGE_KEY = 'bitcoinDB'
 
 export const bitcoinService = {
     getRate,
@@ -7,22 +10,43 @@ export const bitcoinService = {
 }
 
 async function getRate() {
-    const res = await axios.get(
-        'https://blockchain.info/tobtc?currency=USD&value=1'
-    )
-    return res.data
+    const bitcoinDB = storageService.load(STORAGE_KEY) || {}
+    if (!bitcoinDB.rate) {
+        console.warn('Fetching with axios')
+        const res = await axios.get(
+            'https://blockchain.info/tobtc?currency=USD&value=1'
+        )
+        bitcoinDB.rate = res.data
+        storageService.save(STORAGE_KEY, bitcoinDB)
+    }
+
+    return bitcoinDB.rate
 }
 
 async function getMarketPriceHistory() {
-    const res = await axios.get(
-        'https://api.blockchain.info/charts/market-price?timespan=5months&format=json&cors=true'
-    )
-    console.log('res.data:', res.data)
+    const bitcoinDB = storageService.load(STORAGE_KEY) || {}
+    if (!bitcoinDB.history) {
+        console.warn('Fetching with axios')
+        const res = await axios.get(
+            'https://api.blockchain.info/charts/market-price?timespan=5months&format=json&cors=true'
+        )
+        bitcoinDB.history = res.data
+        storageService.save(STORAGE_KEY, bitcoinDB)
+    }
+
+    return bitcoinDB.history
 }
 
 async function getAvgBlockSize() {
-    const res = await axios.get(
-        'https://api.blockchain.info/charts/avg-block-size?timespan=5months&format=json&cors=true'
-    )
-    console.log('res.data:', res.data)
+    const bitcoinDB = storageService.load(STORAGE_KEY) || {}
+    if (!bitcoinDB.avgBlock) {
+        console.warn('Fetching with axios')
+        const res = await axios.get(
+            'https://api.blockchain.info/charts/avg-block-size?timespan=5months&format=json&cors=true'
+        )
+        bitcoinDB.avgBlock = res.data
+        storageService.save(STORAGE_KEY, bitcoinDB)
+    }
+
+    return bitcoinDB.avgBlock
 }
